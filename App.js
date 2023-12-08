4de0070b195245da241b0ebc45184c50b66daec7
Samyukta Kannan <124596951+samyuktaakannan@users.noreply.github.com>
2023-12-08 13:54:37 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import './App.css';
 import NavBar from './Navbar';
 import GalleryPage from './GalleryPage';
 import Contact from './Contact';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ExplorePage from './ExplorePage';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -15,11 +16,20 @@ function App() {
         </video>
         <NavBar />
         <div className="text-overlay">
-          <h1 className="header-text">ENIGMA ESCAPES</h1>
+          <h1 className="header-text">WANDERLOST</h1>
           <p className="description-text">
             Explore the world's hidden treasures, from exotic landscapes to enigmatic cultures. Our travel narratives unravel the secrets of remarkable destinations, guiding you through extraordinary experiences. Embark on a journey of intrigue, wanderlust, and wonder with us, as we decode the enigmas of travel, one escape at a time.
           </p>
+          <Routes>
+            <Route path="/explore" element={<ExplorePage />} />
+          </Routes>
+          <Link to="explore" className="btn">
+            Explore
+          </Link>
         </div>
+        <Routes>
+          <Route path="/App.js" element={<App />} />
+        </Routes>
         <Routes>
           <Route path="/GalleryPage.js" element={<GalleryPage />} />
         </Routes>
